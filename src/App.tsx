a9bc0c003a5216d640ad76052989e0d712ee95ef
Capen <capenr@gmail.com>
2023-11-09 16:36:09 -0500

    Initial check-in; set up Card and Board

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Card({value}: { value: number }) {
+  return <button className="card">{value}</button>
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Card value={1} />
+        <Card value={2} />
+        <Card value={3} />
+      </div>
+    </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
 }
