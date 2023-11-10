7992a5971b73dd73e0038723da3495fdf400f65b
Capen <capenr@gmail.com>
2023-11-10 12:27:04 -0500

    Card flips and flips back on click.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,9 +1,23 @@
+
+import { useState } from 'react';
 import styles from './Card.module.css';
 
 function Card({value}: { value: number }) {
+  const [cardClass, setCardClass] = useState(styles.card);
+  // if the card is not flipped, it will have styles.card as its class
+  const isCardFlipped = cardClass === styles.card ? false : true;
+
+  function onCardClick() {
+    if (!isCardFlipped) {
+      setCardClass(styles.card + " " + styles['card-flip']);
+    } else {
+      setCardClass(styles.card);
+    }
+  }
+
   return (
   <>
-    <div className={styles.card}>
+    <div className={cardClass} onClick={onCardClick}>
       <div className={styles['card-inner']}>
         <div className={styles['card-front']}>
           {value}
